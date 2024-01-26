d5814a3f00ed2942d69940daf3bc154bd33824f5
Adrien Taf's <adrientafitasoa@gmail.com>
2024-01-26 03:40:03 +0100

    mise à jour des liens sociaux

--- src/data/social.jsx
+++ src/data/social.jsx
@@ -7,12 +7,12 @@ export const social = [
     socialIcon: (
       <img className="h-5 w-5 lg:h-10 lg:w-10 fill-accent hover:fill-accent" src={FacebookIcon}/>
     ),
-    socialLink: "https://www.facebook.com/",
+    socialLink: "https://www.facebook.com/adrien.taf",
     socialTitle: "Facebook",
   },
   {
     socialIcon: <img className="h-5 w-5 lg:h-10 lg:w-10 fill-[#3d5a70] hover:fill-accent" src={Github}/>,
-    socialLink: "#",
+    socialLink: "https://github.com/AdrienTafitasoa",
     socialTitle: "Github",
   },
   {
